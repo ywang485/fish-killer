80792802f52d138cad6ad38dfd1fe785ad5085c3
ywang485 <ywang485@gmail.com>
2018-09-30 16:51:49 -0700

    basic scene setup and moving knife with mouse

---
+++ Assets/Scripts/ChefController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChefController : MonoBehaviour {
+
+    public const float knifeXPosMin = -1f;
+    public const float knifeXPosMax = 1f;
+
+    private float gamePlayAreaLeftBoarder;
+    private float gamePlayAreaRightBoarder;
+
+    private GameObject knife;
+
+	// Use this for initialization
+	void Start () {
+        knife = transform.Find("Knife").gameObject;
+        gamePlayAreaLeftBoarder = 0f;
+        gamePlayAreaRightBoarder = Screen.width;
+}
+
+    // Update is called once per frame
+    void Update() {
+        moveKnifeToMousePosition();
+    }
+
+    void moveKnifeToMousePosition() {
+        if (Input.mousePosition.x >= 0 && Input.mousePosition.x <= Screen.width)
+        {
+            Vector3 currKnifePos = knife.transform.position;
+            float mousePosX = Input.mousePosition.x;
+            knife.transform.position = new Vector3(knifeXPosMin + (Input.mousePosition.x / (gamePlayAreaRightBoarder - gamePlayAreaLeftBoarder)) * (knifeXPosMax - knifeXPosMin), currKnifePos.y, currKnifePos.z);
+        }
+    }
+}
