b0a1b4b191b3ea684683fdc002e6e7868f5a7c41
ywang485 <ywang485@gmail.com>
2018-10-26 21:37:56 -0700

    Server code for downloading random motion history

--- AIServer/AIServer.py
+++ AIServer/AIServer.py
@@ -2,12 +2,17 @@ import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
 import time
 import json
+import random
 
 app = Flask(__name__)
 dirPath = 'uploads'
 placeholderHistory = """
-[{"motionType":"Flop1","duration":1}]
+Flop1&1
 """
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    return "hello"
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_motion_history():
@@ -20,12 +25,22 @@ def upload_motion_history():
 
 @app.route('/download-random', methods=['GET', 'POST'])
 def download_random_history():
-    if len(os.listdir(dirPath)):
+    if len(os.listdir(dirPath)) == 0:
         return placeholderHistory
     else:
         random.seed()
-        historyFile = random.choice(os.listdir(dirPath))
-        return historyFile.read()
+        historyFile = open(dirPath + '/' + random.choice([f for f in os.listdir(dirPath) if f.endswith('.json')]), 'r')
+        return convertJSONMotionHistory2EasyParsingForm(json.loads(historyFile.read()))
+
+def convertJSONMotionHistory2EasyParsingForm(motionHistoryInJSON):
+    out = ''
+    motionHistoryInJSON = motionHistoryInJSON['sequence']
+    for m in motionHistoryInJSON:
+        out += m['motionType']
+        out += '&'
+        out += str(m['duration'])
+        out += '\n'
+    return out
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
