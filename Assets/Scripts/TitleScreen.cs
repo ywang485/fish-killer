6b5495d2e6cc25c44c0f8c4f761d64ec5ee389af
Ladace <ladacez@gmail.com>
2018-11-09 02:04:17 +0800

    add title screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+    public Text roomNameUI;
+
+    void Update () {
+        if (ReInput.players.SystemPlayer.GetButtonDown("Restart")) {
+            BootGame.roomName = roomNameUI.text;
+            SceneManager.LoadScene("GamePlay");
+        }
+    }
+}
